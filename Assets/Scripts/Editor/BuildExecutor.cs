c3348a427e5fce4d40039f731302a9e25920729f
tomasz <tomislaw2.0@gmail.com>
2020-01-18 13:55:44 +0100

    Build executor update

--- Assets/Scripts/Editor/BuildExecutor.cs
+++ Assets/Scripts/Editor/BuildExecutor.cs
@@ -1,27 +1,45 @@
 using UnityEditor;
 using UnityEngine;
+using System;
 
 public class BuildExecutor
 {
     public static string[] scenes = { "Assets/Scenes/SampleScene.unity" };
     public static string name = "Venturer";
 
+    private static void Build(string buildName, BuildTarget target)
+    {
+        Console.WriteLine("Current directory: " + Environment.CurrentDirectory.ToString());
+        Console.WriteLine("Building " + buildName);
+        Console.WriteLine("Target: " + target.ToString());
+        var report = BuildPipeline.BuildPlayer(scenes, buildName, target, BuildOptions.None);
+        Console.WriteLine(report.summary.result.ToString());
+        Console.WriteLine(report.summary);
+        Console.WriteLine("File count: " + report.files.Length);
+    }
+
+    public static void Test()
+    {
+        //Todo: delete later
+        Console.WriteLine("Test method");
+    }
+
     [MenuItem("Build/Build WebGL")]
     public static void BuildWebGL()
     {
-        BuildPipeline.BuildPlayer(scenes, "./Build/" + name + "_Web/" + name, BuildTarget.WebGL, BuildOptions.None);
+        Build("./Build/" + name + "_Web/" + name, BuildTarget.WebGL);
     }
 
     [MenuItem("Build/Build Windows")]
     public static void BuildWindows()
     {
-        BuildPipeline.BuildPlayer(scenes, "./Build/" + name + "_Windows/" + name + ".exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+        Build("./Build/" + name + "_Windows/" + name + ".exe", BuildTarget.StandaloneWindows64);
     }
 
     [MenuItem("Build/Build Linux")]
     public static void BuildLinux()
     {
-        BuildPipeline.BuildPlayer(scenes, "./Build/" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64, BuildOptions.None);
+        Build("./Build/" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64);
     }
 
     [MenuItem("Build/Build All")]
