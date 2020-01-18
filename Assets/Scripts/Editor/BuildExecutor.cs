7edd325549d0399b74a79b58b8a220ebba44cc45
tomasz <tomislaw2.0@gmail.com>
2020-01-18 15:11:58 +0100

    More updates to build script - added file logging

--- Assets/Scripts/Editor/BuildExecutor.cs
+++ Assets/Scripts/Editor/BuildExecutor.cs
@@ -4,52 +4,98 @@ using System;
 
 namespace Pipeline
 {
+    public class FileLogger
+    {
+        private string filePath;
+
+        public FileLogger(string path = @"./Build/build.log")
+        {
+            filePath = path;
+            if (System.IO.File.Exists(filePath))
+                System.IO.File.Delete(filePath);
+
+            var dir = System.IO.Path.GetDirectoryName(filePath);
+            if (!System.IO.Directory.Exists(dir))
+                System.IO.Directory.CreateDirectory(dir);
+
+            using (var writer = new System.IO.StreamWriter(System.IO.File.Create(path)))
+            {
+                writer.WriteLine("------ Build log ------");
+                writer.Close();
+            }
+        }
+
+        public void Log(string message)
+        {
+            Debug.Log(message);
+            using (var writer = System.IO.File.AppendText(filePath))
+            {
+                writer.WriteLine(message);
+                writer.Close();
+            }
+        }
+    }
+
     public class BuildExecutor
     {
         public static string[] scenes = { "Assets/Scenes/SampleScene.unity" };
         public static string name = "Venturer";
+        private static FileLogger logger = new FileLogger();
 
-        private static void Build(string buildName, BuildTarget target)
+        private static int Build(string buildName, BuildTarget target)
         {
-            Console.WriteLine("Current directory: " + Environment.CurrentDirectory.ToString());
-            Console.WriteLine("Building " + buildName);
-            Console.WriteLine("Target: " + target.ToString());
-            var report = BuildPipeline.BuildPlayer(scenes, buildName, target, BuildOptions.None);
-            Console.WriteLine(report.summary.result.ToString());
-            Console.WriteLine(report.summary);
-            Console.WriteLine("File count: " + report.files.Length);
+            logger.Log("--- Building " + buildName + " ---");
+            logger.Log("Current directory: " + Environment.CurrentDirectory.ToString());
+
+            UnityEditor.Build.Reporting.BuildReport report;
+            logger.Log("Target: " + target.ToString());
+            try
+            {
+                report = BuildPipeline.BuildPlayer(scenes, buildName, target, BuildOptions.None);
+            }
+            catch (Exception e)
+            {
+                logger.Log(e.ToString());
+                return 1;
+            }
+            logger.Log(report.summary.result.ToString());
+            logger.Log(report.summary.ToString());
+            logger.Log("File count: " + report.files.Length);
+            logger.Log("----------------\n");
+            return 0;
         }
 
-        public static void Test()
+        [MenuItem("Build/af All")]
+        public static int Test()
         {
-            //Todo: delete later
-            Console.WriteLine("Test method");
+            logger.Log("--- Test ---");
+            logger.Log("Test method");
+            logger.Log("Test");
+            return 0;
         }
 
         [MenuItem("Build/Build WebGL")]
-        public static void BuildWebGL()
+        public static int BuildWebGL()
         {
-            Build("./Build/" + name + "_Web/" + name, BuildTarget.WebGL);
+            return Build("./Build/" + name + "_Web/" + name, BuildTarget.WebGL);
         }
 
         [MenuItem("Build/Build Windows")]
-        public static void BuildWindows()
+        public static int BuildWindows()
         {
-            Build("./Build/" + name + "_Windows/" + name + ".exe", BuildTarget.StandaloneWindows64);
+            return Build("./Build/" + name + "_Windows/" + name + ".exe", BuildTarget.StandaloneWindows64);
         }
 
         [MenuItem("Build/Build Linux")]
-        public static void BuildLinux()
+        public static int BuildLinux()
         {
-            Build("./Build/" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64);
+            return Build("./Build/" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64);
         }
 
         [MenuItem("Build/Build All")]
-        public static void BuildAll()
+        public static int BuildAll()
         {
-            BuildLinux();
-            BuildWindows();
-            BuildWebGL();
+            return BuildLinux() | BuildWindows() | BuildWebGL();
         }
     }
 }
