5414afe00f06c04238438fb19fb41c277df4ec9f
tomasz <tomislaw2.0@gmail.com>
2020-01-18 13:33:24 +0100

    Updated build script

--- Assets/Scripts/Editor/BuildExecutor.cs
+++ Assets/Scripts/Editor/BuildExecutor.cs
@@ -9,19 +9,19 @@ public class BuildExecutor
     [MenuItem("Build/Build WebGL")]
     public static void BuildWebGL()
     {
-        BuildPipeline.BuildPlayer(scenes, "./" + name + "_Web/" + name, BuildTarget.WebGL, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, "./Build/" + name + "_Web/" + name, BuildTarget.WebGL, BuildOptions.None);
     }
 
     [MenuItem("Build/Build Windows")]
     public static void BuildWindows()
     {
-        BuildPipeline.BuildPlayer(scenes, "./" + name + "_Windows/" + name, BuildTarget.StandaloneWindows64, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, "./Build/" + name + "_Windows/" + name + ".exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 
     [MenuItem("Build/Build Linux")]
     public static void BuildLinux()
     {
-        BuildPipeline.BuildPlayer(scenes, "./" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, "./Build/" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64, BuildOptions.None);
     }
 
     [MenuItem("Build/Build All")]
