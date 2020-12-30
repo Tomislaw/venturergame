cbc12b98e5f301368666f8ac397cf5ab9fb4a9a3
tomasz <tomislaw2.0@gmail.com>
2020-12-31 00:35:10 +0100

    added grass shader - unfinished

---
+++ Assets/Grass.cs
@@ -0,0 +1,34 @@
+﻿// https://forum.unity.com/threads/compute-shader-for-line-drawing.599989/
+using UnityEngine;
+
+[ExecuteAlways]
+public class Grass : MonoBehaviour
+{
+    public ComputeShader CS;
+
+    public Vector2Int PixelSize;
+    private RenderTexture RT;
+
+    private void Start()
+    {
+        RT = new RenderTexture(PixelSize.x, PixelSize.y, 0);
+        RT.enableRandomWrite = true;
+        RT.format = RenderTextureFormat.ARGB32;
+        RT.filterMode = FilterMode.Point;
+        RT.Create();
+        CS.SetTexture(0, "surface", RT);
+        CS.SetInts("stalks", 16, 32, 16, 100, 48, 32, 10, 200);
+        CS.SetVector("colorA", new Vector4(0, 1, 0, 1));
+        CS.Dispatch(0, RT.width / 8, RT.height / 8, 1);
+        GetComponent<Renderer>().material.mainTexture = RT;
+    }
+
+    private int ahh = 4;
+
+    private void Update()
+    {
+        CS.Dispatch(0, RT.width / 8, RT.height / 8, 1);
+
+        ahh--;
+    }
+}
